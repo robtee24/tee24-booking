dff88349697b67f47519e63df6b1d2241fc649a5
Bret Mette <bret.mette@rowdydesign.com>
2025-11-05 18:39:07 -0500

    loading db url from env

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,18 +1,17 @@
 // prisma/prisma.config.ts
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
-const dbUrl = process.env.DATABASE_URL;
+const url = process.env.DATABASE_URL;
 
-if (!dbUrl) {
-  throw new Error("DATABASE_URL is required");
+if (!url) {
+  throw new Error(
+    "DATABASE_URL is required."
+  );
 }
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: { path: "prisma/migrations" },
-  engine: "classic",
-  datasource: {
-    url: dbUrl,
-  },
+  datasource: { url },
 });
