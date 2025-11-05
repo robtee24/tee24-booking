90bda63ff279a33ebc5d58566535ed11701fc1e9
Robert Gilliam <rob@tee24.golf>
2025-11-05 12:49:38 -0500

    removed middleware.ts

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from 'next/server';
-
-export function middleware(request: NextRequest) {
-  console.log('Incoming request URL:', request.url);
-  console.log('Pathname:', request.nextUrl.pathname);
-  console.log('Full path with query:', request.nextUrl.href);
-
-  return;
-}
