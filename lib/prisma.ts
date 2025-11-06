82b563fb91c751dcd00e1ec0f7a3b785b5b437b2
Bret Mette <bret.mette@rowdydesign.com>
2025-11-06 11:03:25 -0500

    fixed prisma library

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,14 @@
 // lib/prisma.ts
-import { PrismaClient } from '../app/generated/prisma'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
 
-export { prisma }
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
